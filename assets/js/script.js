be81037bdf001292c2c9cee1e301e930527a13fc
Alan Montes de Oca Garcia <esc.amdog5@gmail.com>
2022-11-01 19:49:30 -0600

    Se añadieron palabras para sortear

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ const aviso = document.querySelector('.modal-body');
 var accionAviso = "";
 
 const letrasPermitidas = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-let palabrasSecretas = ['WYILJVAS'];//['UNO', 'DOS', 'TRES', 'CUATRO', 'CINCO', 'SEIS', 'SIETE', 'OCHO', 'NUEVE', 'DIEZ'];
+let palabrasSecretas = ['HTML', 'CSS', 'CELULAR', 'PANTALLA', 'PYTHON', 'PC', 'LAPTOP', 'PHP', 'FRONTEND', 'BACKEND', 'JAVA', 'HDMI', 'WINDOWS', 'LINUX', 'MAC', 'ALURA', 'ORACLE'];
 var palabraElegida = "";
 var letrasPressCorrectas = [];
 var letrasPressIncorrectas = [];
